c4467658e0b303b5477f6f9b832b2b298fd1a2e4
evangray7373@gmail.com <evangray7373@gmail.com>
2023-03-29 23:59:15 -0500

    Can snipe enemies

--- Assets/Scripts/TempWeaponController.cs
+++ Assets/Scripts/TempWeaponController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public enum Weapon
 {
     Whip,
+    Fireball,
 }
 
 public class TempWeaponController : MonoBehaviour
 {
     public static int WhipDamage = 1;
+    public static int FireballDamage = 2;
 }
